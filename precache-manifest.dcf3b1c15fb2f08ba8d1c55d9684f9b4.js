30f0b1b1f1c841923f12b7662e66d41d45f2028a
Luis Osvaldo <os@MacBook-Pro-de-Luis.local>
2019-05-03 09:18:41 -0500

    Updates

---
+++ precache-manifest.dcf3b1c15fb2f08ba8d1c55d9684f9b4.js
@@ -0,0 +1,38 @@
+self.__precacheManifest = [
+  {
+    "revision": "f4f100644a4a3c13d91c316c346d5529",
+    "url": "/bazarMonteVerde/static/media/logoBadge.f4f10064.png"
+  },
+  {
+    "revision": "755830497cfd747a60fef73864080e0a",
+    "url": "/bazarMonteVerde/static/media/logo.75583049.png"
+  },
+  {
+    "revision": "49bab8abed7913c6306bb0222e567084",
+    "url": "/bazarMonteVerde/static/media/fondo.49bab8ab.png"
+  },
+  {
+    "revision": "e47d4a5aceea62dc91d58a01c3f62a89",
+    "url": "/bazarMonteVerde/static/media/bg.e47d4a5a.png"
+  },
+  {
+    "revision": "dcee6d4331ffb69cfd87",
+    "url": "/bazarMonteVerde/static/js/runtime~main.b1dccbee.js"
+  },
+  {
+    "revision": "e5714b161460e432252d",
+    "url": "/bazarMonteVerde/static/js/main.3c3edfb3.chunk.js"
+  },
+  {
+    "revision": "70b9852b6d837a74b146",
+    "url": "/bazarMonteVerde/static/js/2.83e1777e.chunk.js"
+  },
+  {
+    "revision": "e5714b161460e432252d",
+    "url": "/bazarMonteVerde/static/css/main.0fe23fec.chunk.css"
+  },
+  {
+    "revision": "5ab76f0278418d755abc756dd6991e05",
+    "url": "/bazarMonteVerde/index.html"
+  }
+];
